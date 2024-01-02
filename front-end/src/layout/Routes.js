3a87d4eb55b6c2c9384218c5b1da1dd108dfd32e
Austin Doty <asrdoty@gmail.com>
2024-01-02 16:42:26 -0600

    Front End Work. Table and Rows, Edit, Search Seat Table Display and react components. API updating

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -4,7 +4,12 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
+import useQuery from "../utils/useQuery";
 import Reservations from "../reservations/NewReservations";
+import Tables from "../tables/tables";
+import Seat from "../seat/seat";
+import Search from "../search/search";
+import Edit from "../reservations/editReservation";
 
 /**
  * Defines all the routes for the application.
@@ -14,19 +19,33 @@ import Reservations from "../reservations/NewReservations";
  * @returns {JSX.Element}
  */
 function Routes() {
+  const query = useQuery();
+  const date = query.get("date");
   return (
     <Switch>
       <Route exact={true} path="/">
         <Redirect to={"/dashboard"} />
       </Route>
+      <Route exact path="/reservations/:reservation_id/seat">
+        <Seat />
+      </Route>
+      <Route exact path="/reservations/:reservation_id/edit">
+        <Edit />
+      </Route>
+      <Route exact path="/reservations/new">
+        <Reservations />
+      </Route>
       <Route exact={true} path="/reservations">
         <Redirect to={"/dashboard"} />
       </Route>
       <Route path="/dashboard">
-        <Dashboard date={today()} />
+        <Dashboard date={date || today()} />
       </Route>
-      <Route path="/reservations/new">
-        <Reservations />
+      <Route path="/tables/new">
+        <Tables />
+      </Route>
+      <Route path="/search">
+        <Search />
       </Route>
       <Route>
         <NotFound />
